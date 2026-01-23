7af8ecd08d517092678e3663ac0827da86e19a96
NavPilDev <abhinavpillai04@gmail.com>
2026-01-23 02:45:27 -0600

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en" className={plusJakartaSans.variable}>
       <body className={`${plusJakartaSans.className} antialiased`} suppressHydrationWarning>
         {children}
+        <Analytics />
       </body>
     </html>
   );
