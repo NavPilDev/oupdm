9c9103f93bb66def29030a850974fde923223928
NavPilDev <abhinavpillai04@gmail.com>
2026-01-22 22:36:06 -0600

    Added navbar responsiveness and logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -18,9 +20,12 @@ export default function Navbar() {
     }, [isMenuOpen]);
 
     return (
-        <nav className="w-full flex justify-center pt-[20px] pb-0 sticky top-0 z-50 shrink-0 bg-white" data-node-id="3:13">
+        <nav className="w-full flex justify-between items-center px-4 sm:px-18 md:px-[110px] pt-[20px] pb-3 sticky top-0 z-50 shrink-0 bg-[#ffffff]" data-node-id="3:13">
             {/* Desktop Navigation */}
-            <div className="hidden md:flex gap-12 items-center font-bold text-1xl text-black">
+            <div className="hidden md:flex sm:flex justify-center">
+                <Image src="/assets/ocn.png" alt="Ouarin Logo" width={100} height={100} quality={100} className="w-auto h-[50px]" />
+            </div>
+            <div className="hidden lg:flex gap-12 items-center font-bold text-1xl text-black">
                 <a href="/home" className="h-[46px] w-[78px] flex items-center justify-center hover:opacity-70 transition-opacity" data-node-id="3:2">
                     Home
                 </a>
@@ -35,9 +40,13 @@ export default function Navbar() {
                 </a>
             </div>
 
+            <button className="bg-[#5b9acd] px-4 py-4 rounded-full text-white font-bold text-sm hidden md:flex sm:flex">
+                Contact Us
+            </button>
+
             {/* Mobile Menu Button */}
             <button
-                className="md:hidden fixed top-4 left-4 z-50 p-3 bg-white rounded-2xl"
+                className="lg:hidden fixed top-4 left-4 z-50 p-3 bg-white rounded-2xl"
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
                 aria-label="Toggle menu"
             >
@@ -55,6 +64,7 @@ export default function Navbar() {
                             }`}
                     />
                 </div>
+
             </button>
 
             {/* Mobile Menu */}
@@ -63,6 +73,7 @@ export default function Navbar() {
                     }`}
             >
                 <div className="flex flex-col items-center justify-center h-full gap-8">
+                    <Image src="/assets/ocn.png" alt="Ouarin Logo" width={100} height={100} quality={100} className="w-auto h-[50px]" />
                     <a
                         href="/home"
                         className="font-bold text-2xl text-black hover:opacity-70 transition-opacity"
